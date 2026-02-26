63e7195c0bc515fd13f5e4f25a22ff1f9f8a13b4
Ariya <arikianian@gmail.com>
2026-02-26 11:22:25 +0100

    Create hello_world.ps1

---
+++ hello_world.ps1
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Function Hello_world(){
+    #so here you can write any shell command, i use it to take me direcrtly to my c classes repository :]
+    Write-Output "haii :33"; #<- this is a printing command
+}
+
+#function calling
+Hello_world
